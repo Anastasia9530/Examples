335ee82834388f3d4f912a3ddc2f9f3be2aaeafe
Anastasia <N_104@mail.ru>
2022-08-19 13:51:14 +0500

    Рефакторинг

--- src/main/java/org/petrova/yandex/Praktikum4.java
+++ src/main/java/org/petrova/yandex/Praktikum4.java
@@ -6,8 +6,7 @@ public class Praktikum4 {
             int money = 500; // Деньги на корм
             int foodPerDay = 100; // Стоимость пачки корма
             int days = 0; // Дни, когда вы сможете покупать корм
-            for (int i = money-foodPerDay; i >= 0 ; i -= foodPerDay ) {
-                days = days + 3 ;
+            for (int i = money-foodPerDay; i >= 0 ; i -= foodPerDay,days +=3 ) {
                 System.out.println("На " + days+ "-й день останется " + i + " рублей.");
             }
             System.out.println("Денег хватит на " + days+ " дней.");
