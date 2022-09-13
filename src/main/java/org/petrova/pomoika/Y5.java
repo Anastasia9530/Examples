b61883849cf9633e2d9892b169a25509c2c83b64
Anastasia <N_104@mail.ru>
2022-09-13 17:42:09 +0500

    Добавили утилиту

--- src/main/java/org/petrova/pomoika/Y5.java
+++ src/main/java/org/petrova/pomoika/Y5.java
@@ -1,15 +1,18 @@
 package org.petrova.pomoika;
 
+import org.petrova.common.Utils;
+import org.petrova.javarush.Lecture4_6;
+
 public class Y5 {
     public static void main(String[] args) {
         int count = 20;
         float[] array = new float[count];
 
-        printArray(array);
+        Utils.printArray(array);
 
 
         for (int a = 0; a < count; a++) {
-            System.out.println("а= " + a);
+            Utils.log("а= " + a);
 
             if (a >= 15)
                 continue;
@@ -18,27 +21,7 @@ public class Y5 {
             if (a >= 5)
                 array[a] = a;
         }
-        printArray(array);
-
-    }
-
-    public static void printArray(float[] arrayForPrint) { //
-        for (int i = 0; i < arrayForPrint.length; i++) {
-            System.out.print(arrayForPrint[i] + " ");
-
-            switch (i) {
-                case 0:
-                    System.out.print("Начало ");
-                    break;
-                case 1:
-                    System.out.print("Продолжение ");
-                    break;
-                default:
-                    System.out.print(",");
-                    break;
-            }
-
-        }
-        System.out.println();
+        Utils.printArray(array);
+        Lecture4_6.a2();
     }
 }
