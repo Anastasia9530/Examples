defb1053e82c108e996a955c64ff9fd72ffb8a0d
Anastasia <N_104@mail.ru>
2022-09-13 07:45:36 +0500

    Лекция 10

---
+++ src/main/java/org/petrova/javarush/Lecture10_3.java
@@ -0,0 +1,24 @@
+package org.petrova.javarush;
+
+public class Lecture10_3 {
+    public static void main(String[] args) {
+
+        class Person {
+            private String name;
+
+            public Person(String name) {
+                this.name = name;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+        }
+    }
+}
+
+
