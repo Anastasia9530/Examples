eb266456a63d27375eb6d359f21c73f8248ade47
Anastasia <N_104@mail.ru>
2022-09-29 19:15:21 +0500

    Лекция 10 /  метод equals / hashCode

---
+++ src/main/java/org/petrova/javarush/Lecture10_4.java
@@ -0,0 +1,10 @@
+package org.petrova.javarush;
+//Оператор instanceof
+public class Lecture10_4 {
+    public static void main(String[] args) {
+
+        Integer x = new Integer(22);
+
+        System.out.println(x instanceof Integer);
+    }
+}
