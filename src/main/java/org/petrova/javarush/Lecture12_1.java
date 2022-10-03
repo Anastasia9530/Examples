a507a66268e500125ffce9949dd538145281df39
Anastasia <N_104@mail.ru>
2022-10-03 21:47:23 +0500

    Лекция 12_1 /  Autoboxing и unboxing

---
+++ src/main/java/org/petrova/javarush/Lecture12_1.java
@@ -0,0 +1,10 @@
+package org.petrova.javarush;
+
+public class Lecture12_1 { // Autoboxing и unboxing
+    public static void main(String[] args) {
+        Integer a = 5;
+        int b = a;
+        Integer c = a + b;
+        int d = a + b + c;
+    }
+}
