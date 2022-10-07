49972e089c311d90c8711db9c4cfed5d587903b7
Anastasia <N_104@mail.ru>
2022-10-07 21:54:34 +0500

    Лекция 13_4 / Новый тип данных – enum: перечисление

---
+++ src/main/java/org/petrova/javarush/Lecture13_4.java
@@ -0,0 +1,16 @@
+package org.petrova.javarush;
+
+public class Lecture13_4 {
+    public static void main(String[] args) {
+        enum Day // Новый тип Day
+        {
+            MONDAY,
+            TUESDAY,
+            WEDNESDAY,
+            THURSDAY,
+            FRIDAY,
+            SATURDAY,
+            SUNDAY
+        }
+    }
+}
