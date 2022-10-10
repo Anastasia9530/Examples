0f4e9749ed34913b9fbda52425730b8da4b6eb74
Anastasia <N_104@mail.ru>
2022-10-10 21:30:04 +0500

    Лекция 14_1 / Несколько блоков catch

---
+++ src/main/java/org/petrova/javarush/Lecture14_1.java
@@ -0,0 +1,22 @@
+package org.petrova.javarush;
+
+public class Lecture14_1 { // Несколько блоков catch
+    public static void main(String[] args) {
+        System.out.println("Начало метода main");
+        try {
+            calculate(0);
+        } catch (ArithmeticException e) {
+            System.out.println("Было деление на 0");
+        } catch (Exception e) {
+            System.out.println("Перехвачено какое-то исключение");
+        }
+
+        System.out.println("Конец метода main");
+    }
+
+    public static void calculate(int n) {
+        System.out.println("calculate начало: " + n);
+        System.out.println(2 / n);
+        System.out.println("calculate конец: " + n);
+    }
+}
