fc434cc4834c4222c225455ad5b7b4f70aea74c0
Anastasia <N_104@mail.ru>
2022-11-06 22:42:38 +0500

    Философия Java / Повторение лекций на инкремент,рандом

---
+++ src/main/java/org/petrova/pomoika/Y10.java
@@ -0,0 +1,47 @@
+package org.petrova.pomoika;
+
+import org.petrova.common.Utils;
+
+import java.util.Random;
+
+public class Y10 {
+    public static void main(String[] args) {
+        Utils.log("Привет");
+
+        a1();
+        a2();
+        a3();
+        a4();
+    }
+
+    public static void a1() {
+        Random rand = new Random(77);
+        int a, c, z;
+        a = rand.nextInt(100) + 1;
+        Utils.log("a: " + a);
+        c = rand.nextInt(100) + 1;
+        Utils.log("c: " + c);
+        z = a + c;
+        Utils.log("z: " + z);
+    }
+
+    public static void a2() {
+        int a = 10;
+        Utils.log("a++ : " + a++);// после вып.28 строки а=6,но печатаем a=5
+        Utils.log("a++: " + a++);//после вып.29 строки а=7,но печатаем a=6
+        Utils.log("++a : " + ++a);
+        Utils.log("--a : " + --a);
+    }
+
+    public static void a3() {
+        int a = 10;
+        Utils.log(" : " + (--a + --a + a + a++ + a++ + a));// 52
+        Utils.log(" : " + --a + --a + a + a++ + a++ + a); // 9888910
+    }
+
+    public static void a4() {
+        int a = 3;
+        var str = " " + a + (a++ - a) + ++a + (a++ + --a) + ++a + a;
+        Utils.log(str);
+    }
+}
