c41e2101ba73777766b24d1f7462c355ddf69ab7
Anastasia <N_104@mail.ru>
2022-11-08 14:03:57 +0500

    Добавление метода печати

--- src/main/java/org/petrova/common/Utils.java
+++ src/main/java/org/petrova/common/Utils.java
@@ -5,12 +5,16 @@ public class Utils {
     public static void log(String str) {
         System.out.println(str);
     }
+    public static void print(String str) {
+        System.out.print(str);
+    }
 
     public static void printArray(float[] arrayForPrint) { //
         for (int i = 0; i < arrayForPrint.length; i++)
             System.out.print(arrayForPrint[i] + " ");
 
         Utils.log("");
+        Utils.print(" ");
     }
 
 
