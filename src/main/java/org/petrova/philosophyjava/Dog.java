16c2462e68854968847110318113b513dc3fcf3b
Anastasia <N_104@mail.ru>
2022-11-08 14:02:48 +0500

    Философия Java / Повторение лекций / Static / Операторы сравнения

---
+++ src/main/java/org/petrova/philosophyjava/Dog.java
@@ -0,0 +1,26 @@
+package org.petrova.philosophyjava;
+
+import org.petrova.common.Utils;
+
+public class Dog {
+    String name ;
+    String says ;
+    public static void main(String[]args){
+
+        Dog dog1 = new Dog();
+        Dog dog2 = new Dog();
+
+        dog1.name = "Spot";
+        dog1.says = "Gav";
+        dog2.name = "Scruffy";
+        dog2.says= "Rrr";
+
+        Utils.log("name: "+ dog1.name+","+"says:" + dog1.says);
+        Utils.log("name: "+ dog2.name+","+"says:" + dog2.says);
+
+        Dog spot = new Dog();
+        System.out.println( spot == dog1);
+        System.out.println( spot != dog1);
+        System.out.println (spot.equals(dog1));
+    }
+}
