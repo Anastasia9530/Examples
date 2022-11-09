c90344c777aa31dc1d43e450e85ef73cfef9314d
Anastasia <N_104@mail.ru>
2022-11-09 21:42:52 +0500

    М. Э. Абрамян / 1000 задач по программированию / For

---
+++ src/main/java/org/petrova/tasksAbramyan/Task_For.java
@@ -0,0 +1,29 @@
+package org.petrova.tasksAbramyan;
+
+import org.petrova.common.Utils;
+
+public class Task_For {
+    public static void main(String[] args) {
+
+        //Даны целые числа N и K (K > 0). Вывести K раз число N.
+
+        for (int k = 0; k < 7; k++) {
+            System.out.println("n");
+        }
+
+        a1();
+    }
+
+    public static void a1() {
+
+//        Дано вещественное число — цена 1 кг конфет. Вывести стоимость 1,
+//        2, …, 10 кг конфет.
+
+        int kg = 1;
+        for (int price = 1; price < 11; price++) {
+            System.out.print("Цена " + kg++);
+            System.out.print("kg конфет: " + price);
+            Utils.log("");
+        }
+    }
+}
