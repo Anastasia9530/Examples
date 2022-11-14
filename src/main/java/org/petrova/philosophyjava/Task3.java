52d2f14196200c2c5d5a08cad1e35e9191ac1e43
Anastasia <N_104@mail.ru>
2022-11-14 20:06:33 +0500

    Философия Java / Повторение конструкторов

---
+++ src/main/java/org/petrova/philosophyjava/Task3.java
@@ -0,0 +1,12 @@
+package org.petrova.philosophyjava;
+
+import org.petrova.common.Utils;
+
+public class Task3 {
+
+    // Конструкторы могут получать аргументы *
+
+    Task3(int i) {
+        Utils.log("Task3 " + i);
+    }
+}
---
+++ src/main/java/org/petrova/philosophyjava/Task3_0.java
@@ -0,0 +1,9 @@
+package org.petrova.philosophyjava;
+
+public class Task3_0 {
+    public static void main(String[]args){
+        for (int i=0; i<9; i++){
+            new Task3(i);
+        }
+    }
+}
