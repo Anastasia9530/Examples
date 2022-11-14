bcb5b01ed050a8de3402ed6058cf9e4b72988275
Anastasia <N_104@mail.ru>
2022-11-14 20:05:48 +0500

    Философия Java / Повторение цикла for,команды break

---
+++ src/main/java/org/petrova/philosophyjava/Task2.java
@@ -0,0 +1,21 @@
+package org.petrova.philosophyjava;
+
+import org.petrova.common.Utils;
+
+public class Task2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 1000; i++) {
+            if (i == 99) break;
+            Utils.log(i + " ");
+
+            a1();
+        }
+    }
+
+    public static void a1() {
+        // Бесконечный цикл
+        for (int i = 0; i < i + 1; i++) {
+            Utils.log(i + " ");
+        }
+    }
+}
