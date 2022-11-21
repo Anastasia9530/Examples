79034e77e5ff7f09de79850c74fb5eee14246735
Anastasia <N_104@mail.ru>
2022-11-21 21:40:34 +0500

    Повторение исключений

--- src/main/java/org/petrova/pomoika/Y14.java
+++ src/main/java/org/petrova/pomoika/Y14.java
@@ -2,6 +2,9 @@ package org.petrova.pomoika;
 
 import org.petrova.common.Utils;
 
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Y14 {
@@ -14,10 +17,54 @@ public class Y14 {
             Scanner in = new Scanner(System.in);
             Utils.log("Введите число: ");
             int number = in.nextInt();
-            Utils.log(""+number);
+            Utils.log("" + number);
 
-        } catch (Exception e) {
-            Utils.log("Указатель не может указывать на null!");
+            a1(number);
+
+        } catch (InputMismatchException e) {
+            Utils.log("Указатель не может указывать на null1!");
+            Utils.log(" Ошибка: " + e);
+            e.printStackTrace();
+        } catch (NoSuchElementException e) {
+            Utils.log("Указатель не может указывать на null2!");
+            Utils.log(" Ошибка: " + e);
+            e.printStackTrace();
+        } catch (IllegalStateException e) {
+            Utils.log("Указатель не может указывать на null3!");
+            Utils.log(" Ошибка: " + e);
+            e.printStackTrace();
+        } catch (IOException e) {
+            Utils.log("Ошибка не критична");
+        }
+        catch (NastyaError error) {
+            Utils.log("Ошибка критична: "+ error.getMessage());
+            System.exit(0);
+
+        }
+        finally {
+            Utils.log("Мы попали в блок finally");
+        }
+
+        Utils.log("Успешное завершение программы");
+    }
+
+
+    public static void a1(int number) throws IOException {
+
+        if (number == 10) throw new NullPointerException();
+        if (number == 20) throw new IOException();
+        if (number == 50) throw new NastyaError("50 слишком много");
+
+    }
+
+    static class NastyaError extends Error {
+
+        public NastyaError() {
+
+        }
+
+        public NastyaError(String message) {
+            super(message);
         }
     }
 }
