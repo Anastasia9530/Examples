f14ba8ebc3282a529e0a5d0b28ec2de56e410548
Anastasia <N_104@mail.ru>
2022-12-07 18:30:23 +0500

    Философия Java / Создание объекта внутреннего класса

--- src/main/java/org/petrova/philosophyjava/Task3_0.java
+++ src/main/java/org/petrova/philosophyjava/Task3_0.java
@@ -1,7 +1,19 @@
 package org.petrova.philosophyjava;
 
 public class Task3_0 {
+    public class Class{}
+
     public static void main(String[] args) {
+
+        // Прямое создание объекта внутреннего класса с синтаксисом new
+
+       Task3_0 n = new Task3_0();
+       Task3_0.Class n1= n.new Class();
+
+        a1();
+    }
+
+    public static void a1() {
         for (int i = 0; i < 9; i++) {
             new Task3(i);
         }
